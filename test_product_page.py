c0cdd8ad7c2c040d0bcdba0d5865494eae2ca2ca
b1gslick <s_timokhin@wargaming.net>
2022-09-26 11:00:15 +0300

    prepare for review

--- test_product_page.py
+++ test_product_page.py
@@ -15,6 +15,7 @@ product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-w
 urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', urls)
 @pytest.mark.usefixtures('browser')
 def test_guest_can_add_product_to_basket(browser, link):
@@ -63,6 +64,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 @pytest.mark.usefixtures('browser')
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -71,6 +73,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 @pytest.mark.usefixtures('browser')
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -104,6 +107,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     @pytest.mark.usefixtures('setup')
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
